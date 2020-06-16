8651b6e553eb8830799e38e6c0c275974ec461e1
guominghao <guominghao@sensetime.com>
2020-06-16 22:54:10 +0800

    add training code; WIP: ongoing training to validate the results

--- experiments/RobNet_large_cifar10/config.py
+++ experiments/RobNet_large_cifar10/config.py
@@ -1,3 +1,4 @@
+# Model Params
 model = 'robnet_large_v1'
 model_param = dict(C=36,
                    num_classes=10,
@@ -7,15 +8,26 @@ model_param = dict(C=36,
                    stem_multiplier=3,
                    share=True,
                    AdPoolSize=1)
+
+# Dataset Params
 dataset = 'cifar10'
-dataset_param = dict(data_root='../data/cifar10',
-                     batch_size=32,
+dataset_param = dict(data_root='./data/cifar10',
+                     cutout=True,
+                     batch_size=48, # With a world_size of 32, the total batch_size is 1536 for training
                      num_workers=2)
 report_freq = 10
 seed = 10
-gpu = 1
 save_path = './log'
-resume_path = './checkpoint/RobNet_large_v1_cifar10.pth.tar'
+# resume_path = './checkpoint/RobNet_large_v1_cifar10.pth.tar'
+
+# Train Params
+train_param = dict(learning_rate=1.2,
+                   learning_rate_min=0.001,
+                   momentum=0.9,
+                   weight_decay=1e-4,
+                   epochs=150,
+                   no_wd=True,
+                   warm_up_param=dict(warm_up_base_lr=0.02, warm_up_epochs=20))
 
 # Attack Params
 attack_param = {'attack': True,
--- main.py
+++ main.py
@@ -6,44 +6,37 @@ import os
 import argparse
 import pprint
 
-import utils
 import logging
 import time
 import glob
 import shutil
 from mmcv import Config
+from mmcv.runner import init_dist, get_dist_info
 
 import architecture_code
 import models
 from dataset import dataset_entry
 from attack import *
+import utils
+import lr_scheduler
 
-Debug = False
+Debug = True
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', '-c', type=str, default='./experiments/RobNet_free_cifar10/config.py',
                     help='location of the config file')
+parser.add_argument('--distributed', action='store_true', default=False, help='Distributed training')
+parser.add_argument('--eval_only', action='store_true', default=False, help='Only evaluate')
 parser.set_defaults(augment=True)
 args = parser.parse_args()
 
 
 def main():
-    global cfg
+    global cfg, rank, world_size
 
     cfg = Config.fromfile(args.config)
 
-    cfg.save = '{}/{}-{}-{}'.format(cfg.save_path, cfg.model, cfg.dataset,
-                                    time.strftime("%Y%m%d-%H%M%S"))
-    utils.create_exp_dir(cfg.save)
-
-    logger = utils.create_logger('global_logger', cfg.save + '/log.txt')
-
-    if not torch.cuda.is_available():
-        logger.info('no gpu device available')
-        sys.exit(1)
-
-    # Set cuda device & seed
-    torch.cuda.set_device(cfg.gpu)
+    # Set seed
     np.random.seed(cfg.seed)
     cudnn.benchmark = True
     torch.manual_seed(cfg.seed)
@@ -54,30 +47,134 @@ def main():
     print('==> Building model..')
     arch_code = eval('architecture_code.{}'.format(cfg.model))
     net = models.model_entry(cfg, arch_code)
+    rank = 0 # for non-distributed
+    world_size = 1 # for non-distributed
+    if args.distributed:
+        print('==> Initializing distributed training..')
+        init_dist(launcher='slurm', backend='nccl') # Only support slurm for now, if you would like to personalize your launcher, please refer to https://github.com/open-mmlab/mmcv/blob/master/mmcv/runner/dist_utils.py
+        rank, world_size = get_dist_info()
     net = net.cuda()
 
     cfg.netpara = sum(p.numel() for p in net.parameters()) / 1e6
-    logger.info('config: {}'.format(pprint.pformat(cfg)))
 
     # Load checkpoint.
-    if not Debug:
+    if cfg.get('resume_path', False):
         print('==> Resuming from checkpoint..')
-        utils.load_state(cfg.resume_path, net)
+        utils.load_state(cfg.resume_path, net, rank=rank)
+    if args.distributed:
+        net = torch.nn.parallel.DistributedDataParallel(net, device_ids=[torch.cuda.current_device()], output_device=torch.cuda.current_device())
 
     # Data
     print('==> Preparing data..')
-
-    testloader = dataset_entry(cfg)
+    trainloader, testloader, train_sampler, test_sampler = dataset_entry(cfg, args.distributed)
     criterion = nn.CrossEntropyLoss()
+    if not args.eval_only:
+        cfg.attack_param.num_steps = 7
     net_adv = AttackPGD(net, cfg.attack_param)
 
-    print('==> Testing on Clean Data..')
-    test(net, testloader, criterion)
+    # Train params
+    print('==> Setting train parameters..')
+    train_param = cfg.train_param
+    epochs = train_param.epochs
+    init_lr = cfg.train_param
+    if train_param.get('warm_up_param', False):
+        warm_up_param = train_param.warm_up_param
+        init_lr = warm_up_param.warm_up_base_lr
+        epochs += warm_up_param.warm_up_epochs
+    if train_param.get('no_wd', False):
+        param_group, type2num, _, _ = utils.param_group_no_wd(net)
+        cfg.param_group_no_wd = type2num
+        optimizer = torch.optim.SGD(param_group, lr=init_lr, momentum=train_param.momentum, weight_decay=train_param.weight_decay)
+    else:
+        optimizer = torch.optim.SGD(net.parameters(), lr=init_lr, momentum=train_param.momentum, weight_decay=train_param.weight_decay)
 
-    print('==> Testing on Adversarial Data..')
-    test(net_adv, testloader, criterion, adv=True)
+    #TODO check if no warm_up
+    scheduler = lr_scheduler.CosineLRScheduler(optimizer, epochs, train_param.learning_rate_min, init_lr, train_param.learning_rate, (warm_up_param.warm_up_epochs if train_param.get('warm_up_param', False) else 0))
+    # Log
+    print('==> Writing log..')
+    if rank == 0:
+        cfg.save = '{}/{}-{}-{}'.format(cfg.save_path, cfg.model, cfg.dataset,
+                                    time.strftime("%Y%m%d-%H%M%S"))
+        utils.create_exp_dir(cfg.save)
+        logger = utils.create_logger('global_logger', cfg.save + '/log.txt')
+        logger.info('config: {}'.format(pprint.pformat(cfg)))
+
+    # Evaluation only
+    if args.eval_only:
+        assert (not cfg.get('resume_path', False)), 'Should set the resume path for the eval_only mode'
+        print('==> Testing on Clean Data..')
+        test(net, testloader, criterion)
+        print('==> Testing on Adversarial Data..')
+        test(net_adv, testloader, criterion, adv=True)
+        return
+
+    # Training process
+    for epoch in range(epochs):
+        train_sampler.set_epoch(epoch)
+        test_sampler.set_epoch(epoch)
+        scheduler.step()
+        if rank == 0:
+            logger.info('Epoch %d learning rate %e', epoch, scheduler.get_lr()[0])
+        
+        # Train for one epoch
+        train(trainloader, net_adv, criterion, optimizer)
+
+        # Validate for one epoch
+        valid_acc = test(testloader, net_adv, criterion, adv=True)
+
+        if rank == 0:
+            logger.info('Validation Accuracy', valid_acc)
+            is_best = valid_acc > best_acc
+            best_acc = max(valid_acc, best_acc)
+            print('==> Saving')
+            state = {'epoch': epoch,
+                     'best_acc': best_acc,
+                     'optimizer': optimizer.state_dict(),
+                     'state_dict': net.state_dict(),
+                     'scheduler': scheduler} # TODO check the schedu;er
+            utils.save_checkpoints(state, is_best, os.path.join(cfg.save))
+    # TODO check all there results on single card
 
 
+def train(trainloader, net, criterion, optimizer):
+    objs = utils.AverageMeter(cfg.report_freq)
+    top1 = utils.AverageMeter(cfg.report_freq)
+    top5 = utils.AverageMeter(cfg.report_freq)
+    
+    logger = logging.getLogger('global_logger')
+
+    for step, (inputs, targets) in enumerate(trainloader):
+        net.train()
+        num = inputs.size(0)
+        inputs, targets = inputs.cuda(), targets.cuda()
+        
+        logits, _ = net(inputs, targets)
+        loss = criterion(logits, targets)
+        prec1, prec5 = utils.accuracy(logits, targets, topk=(1, 5))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        reduced_loss = loss.clone()
+        reduced_prec1 = prec1.clone() / world_size
+        reduced_prec5 = prec5.clone() / world_size
+        if args.distributed:
+            torch.distributed.all_reduce(reduced_loss)
+            torch.distributed.all_reduce(reduced_prec1)
+            torch.distributed.all_reduce(reduced_prec5)
+        objs.update(reduced_loss.clone().item())
+        top1.update(reduced_prec1.clone().item())
+        top5.update(reduced_prec5.clone().item())
+
+        if step % cfg.report_freq == 0 and rank == 0:
+            logger.info('Iter: [{0}/{1}]\t'
+                        'Loss: {loss.val:.4f} ({loss.avg:.4f})\t'
+                        'Prec@1: {top1.val:.3f} ({top1.avg:.3f})\t'
+                        'Prec@5: {top5.val:.3f} ({top5.avg:.3f})\t'
+                        .format(step, len(trainloader), loss=objs, top1=top1, top5=top5))
+
+        
 def test(net, testloader, criterion, adv=False):
     losses = utils.AverageMeter(0)
     top1 = utils.AverageMeter(0)
@@ -100,21 +197,30 @@ def test(net, testloader, criterion, adv=False):
             prec1, prec5 = utils.accuracy(outputs.data, targets, topk=(1, 5))
 
             num = inputs.size(0)
-            losses.update(loss.item(), num)
-            top1.update(prec1.item(), num)
-            top5.update(prec5.item(), num)
-
-            if batch_idx % cfg.report_freq == 0:
+            losses.update(loss.clone().item(), num)
+            top1.update(prec1.clone().item(), num)
+            top5.update(prec5.clone().item(), num)
+            
+            if batch_idx % cfg.report_freq == 0 and rank == 0:
                 logger.info(
                     'Test: [{0}/{1}]\t'
-                    'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                    'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                    'Prec@5 {top5.val:.3f} ({top5.avg:.3f})\t'
+		    'Loss: {loss.val:.4f} ({loss.avg:.4f})\t'
+		    'Prec@1: {top1.val:.3f} ({top1.avg:.3f})\t'
+		    'Prec@5: {top5.val:.3f} ({top5.avg:.3f})\t'
                     .format(batch_idx, len(testloader), loss=losses, top1=top1, top5=top5))
 
-    final_loss = losses.avg
-    final_top1 = top1.avg
-    final_top5 = top5.avg
+    final_loss_sum = torch.Tensor([losses.sum]).cuda()
+    final_top1_sum = torch.Tensor([top1.sum]).cuda()
+    final_top5_sum = torch.Tensor([top5.sum]).cuda()
+    total_num = torch.Tensor([losses.count]).cuda()
+    if args.distributed:
+        torch.distributed.all_reduce(final_loss_sum)
+        torch.distributed.all_reduce(final_top1_sum)
+        torch.distributed.all_reduce(final_top5_sum)
+        torch.distributed.all_reduce(total_num)
+    final_loss = final_loss_sum.item() / total_num.item()
+    final_top1 = final_top1_sum.item() / total_num.item()
+    final_top5 = final_top5_sum.item() / total_num.item()
 
     logger.info(' * Prec@1 {:.3f}\tPrec@5 {:.3f}\tLoss {:.3f}\t'.format(final_top1, final_top5, final_loss))
 
